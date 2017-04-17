4b2244f816aacedb0a47637f7c45f1d431ff3ced
Shionn (Kepka Ludovic) <shionn@gmail.com>
2017-04-17 10:18:45 +0200

    Mode de cropping pour les images

--- src/main/java/shionn/blog/content/ImageController.java
+++ src/main/java/shionn/blog/content/ImageController.java
@@ -30,38 +30,45 @@ public class ImageController {
 	@RequestMapping(path = { "wp-content/uploads/{img:.*}.png",
 			"img/{img:.*}.png" }, method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
 	public byte[] png(@PathVariable("img") String imgName, @RequestParam(name = "w", defaultValue = "0") int w,
-			@RequestParam(name = "h", defaultValue = "0") int h)
+			@RequestParam(name = "h", defaultValue = "0") int h,
+			@RequestParam(name = "crop", defaultValue = "false") boolean crop)
 			throws IOException, IM4JavaException, InterruptedException {
-		return read(imgName, w, h, ".png");
+		return read(imgName, w, h, crop, ".png");
 	}
 
 	@ResponseBody
 	@RequestMapping(path = { "wp-content/uploads/{img:.*}.jpg",
 			"img/{img:.*}.jpg" }, method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
 	public byte[] jpeg(@PathVariable("img") String imgName, @RequestParam(name = "w", defaultValue = "0") int w,
-			@RequestParam(name = "h", defaultValue = "0") int h)
+			@RequestParam(name = "h", defaultValue = "0") int h,
+			@RequestParam(name = "crop", defaultValue = "false") boolean crop)
 			throws IOException, InterruptedException, IM4JavaException {
-		return read(imgName, w, h, ".jpg");
+		return read(imgName, w, h, crop, ".jpg");
 	}
 
 	@ResponseBody
 	@RequestMapping(path = { "wp-content/uploads/{img:.*}.bmp",
 			"img/{img:.*}.bmp" }, method = RequestMethod.GET, produces = "image/bmp")
 	public byte[] bmp(@PathVariable("img") String imgName, @RequestParam(name = "w", defaultValue = "0") int w,
-			@RequestParam(name = "h", defaultValue = "0") int h)
+			@RequestParam(name = "h", defaultValue = "0") int h,
+			@RequestParam(name = "crop", defaultValue = "false") boolean crop)
 			throws IOException, InterruptedException, IM4JavaException {
-		return read(imgName, w, h, ".bmp");
+		return read(imgName, w, h, crop, ".bmp");
 	}
 
-	private byte[] read(String imgName, int w, int h, String ext)
+	private byte[] read(String imgName, int w, int h, boolean crop, String ext)
 			throws IOException, InterruptedException, IM4JavaException, FileNotFoundException {
 		String wanted = imgSourceFolder + imgName + ext;
 		if (w > 0 && h > 0) {
-			wanted = imgSourceFolder + imgName + "-" + w + "-" + h + ext;
+			wanted = imgSourceFolder + imgName + "-" + w + "-" + h + (crop ? "-crop" : "") + ext;
 			if (!new File(wanted).exists()) {
 				IMOperation op = new IMOperation();
 				op.addImage(imgSourceFolder + imgName + ext);
-				op.resize(w, h);
+				if (crop) {
+					op.resize(w, h, "^").gravity("center").extent(w, h);
+				} else {
+					op.resize(w, h);
+				}
 				op.addImage(wanted);
 				new ConvertCmd().run(op);
 			}
