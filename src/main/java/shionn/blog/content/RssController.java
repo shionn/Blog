3264388dbcec745a54d770824ba9dd77a36979a3
Shionn (Kepka Ludovic) <shionn@gmail.com>
2017-05-14 09:45:54 +0000

    Amelioration des flux XML

--- src/main/java/shionn/blog/content/RssController.java
+++ src/main/java/shionn/blog/content/RssController.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
