843623f38ae435f79b654e4a9756938db470fb86
Shionn <shionn@gmail.com>
2017-07-04 18:34:52 +0200

    Correction Flux RSS

--- src/main/java/shionn/blog/db/dao/RssDao.java
+++ src/main/java/shionn/blog/db/dao/RssDao.java
@@ -17,15 +17,15 @@ import shionn.blog.db.dbo.Post;
  */
 public interface RssDao {
 
-	@Select("SELECT p.url, p.title, p.published, p.content, u.name, u.email "
+	@Select("SELECT p.url, p.title, p.published, p.content, u.name AS author_name, u.email AS author_email "
 			+ "FROM post AS p "
 			+ "LEFT JOIN user AS u ON p.author = u.id "
 			+ "WHERE p.status = 'publish' "
 			+ "AND p.type = 'post' "
 			+ "ORDER BY p.published DESC "
 			+ "LIMIT 10")
-	@Results({ @Result(column = "u.name", property = "author.name"),
-			@Result(column = "u.email", property = "author.email") })
+	@Results({ @Result(column = "author_name", property = "author.name"),
+			@Result(column = "author_email", property = "author.email") })
 	List<Post> readLastPosts();
 
 	@Select("SELECT c.author_name, c.author_email, c.date, c.content,  p.title, p.url "
